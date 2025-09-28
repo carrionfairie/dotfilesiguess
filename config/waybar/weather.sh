daf387731694a30408293348499c662d1353d68b
jebedia jungus <jebedia@jungus.com>
2025-09-28 01:25:03 -0300

    oops

--- config/waybar/weather.sh
+++ config/waybar/weather.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 #sleep 2s; echo 24°C ☀️
 
-#msg=$(echo $(curl 'wttr.in/?format=%tAND%c') | sed 's/AND/ /g; s/+//g')
+msg=$(echo $(curl 'wttr.in/?format=%tAND%c') | sed 's/AND/ /g; s/+//g')
 
 if [[ $msg == *"Unknown location"* ]]; then
   exit
